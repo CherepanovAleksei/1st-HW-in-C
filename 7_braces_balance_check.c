af2c8ff8d43576b584531befc8a3bd72cada7fce
CherepanovAleksei <cherepanov.alexei.olegovich@gmail.com>
2016-10-19 16:51:08 +0300

    Update 7_braces_balance_check.c

--- 7_braces_balance_check.c
+++ 7_braces_balance_check.c
@@ -1,30 +1,46 @@
+/** program #7:braces balance check
+ *
+ *
+ *  by Cherepanov Alexei (PI-171)
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
 
-	char str1[10000];
-	int i, a=0;
+	char str1[10000];   //обрабатываемая строка
+	int i;              //счетчик
+	int flag = 0;       //флаг проверки
 	gets(str1);
-	for (i = 0; i <= strlen(str1); i++)
+	for (i = 0; i <= strlen(str1); i++)     //обрабатываем каждый символ
 	{
-		if (str1[i] == '(') a++;
-		if (str1[i] == ')') a--;
-		if (a<0)
+		if (str1[i] == '(')
+        	{
+        	    flag++;
+        	}
+
+		if (str1[i] == ')')
+        	{
+        	    flag--;
+        	}
+
+		if (flag < 0)  //роверяем порядок
 		{
-			printf("\nno");
-			break;
-		};
-	};
-	if (a==0)
-	{
-		printf("\nAll right");
+			printf("no");
+			goto label;  //breake+пропуск проверки флага
+		}
 	}
-	else
+	if (flag == 0)    //проверяем баланс
 	{
-		printf("\nno");
+		printf("All right");
 	}
+  	else
+	{
+		printf("no");
+	}
+	label:
 	printf("\n");
 	system("pause");
 	return 0;
