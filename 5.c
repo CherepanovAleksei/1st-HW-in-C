8e8036890e986dfe2b4f709a32a388c666dfe805
CherepanovAleksei <cherepanov.alexei.olegovich@gmail.com>
2016-11-11 01:36:41 +0300

    Update 5.c

--- 5.c
+++ 5.c
@@ -1,25 +1,37 @@
+/** program #5:array m+n>>n+m
+ *
+ *
+ *  by Cherepanov Alexei (PI-171)
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-    	setlocale(0, "");
-	int i, j, m, n;
-	scanf("%d%d",&m,&n);
+	int i; //счетчик
+	int j;
 
-	int mas[1000]; // Выделение памяти для массива
+	int m; //длина первого отрезка
+	int n; //длина второго отрезка
+	scanf("%d%d",&m,&n); //задаем длину первого и второг отрезка
 
-	for (i = 1; i <= m+n; i++) scanf("%d", &mas[i]);
+	int array[1000]; // Выделение памяти для массива
+
+	for (i = 1; i <= m+n; i++) scanf("%d", &array[i]); //считываем-заполняем массив
 	for ( j = 1; j <= n; j++)
 	{
 		for (i = m+j; i >= 1+j; i--)
 		{
-			mas[i] = mas[i]+mas[i - 1];
-			mas[i - 1] = mas[i] - mas[i-1];
-			mas[i] = mas[i]-mas[i - 1];
+			array[i] += array[i - 1]; //прибавляем предыдущий эл
+			array[i - 1] = array[ i ] - array[i - 1];  //присваиваем разницу
+			array[i] -= array[i - 1]; //вычитаем
 		}
 	}
-	for ( i = 1; i <= m + n; i++) printf("%d ", mas[i]);
+	for ( i = 1; i <= m + n; i++)
+    {
+        printf("%d ", array[i]); //выводим
+    }
 	printf("\n");
 	system("pause");
 	return 0;
